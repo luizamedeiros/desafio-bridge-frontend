2a994660f5ac4e3f70af04d07d3081ff6d9a6f6e
luizamedeiros <65048102+luizamedeiros@users.noreply.github.com>
2022-08-09 11:39:39 -0300

    Create History page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {Routes, Route, BrowserRouter as Router} from 'react-router-dom';
 import Home from './pages/Home';
+import History from './pages/History';
 
 function App() {
 return(
@@ -8,11 +9,12 @@ return(
   <ul>
     <li><a href='http://localhost:3000/'>Home</a></li>
     <li className='headerText'> desafio bridge_ </li>
-    <li><a >History</a></li>
+    <li><a href='http://localhost:3000/history'>History</a></li>
   </ul>
   <Router>
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/history" element={<History/>}/>
     </Routes>
   </Router>
   </>
---
+++ src/pages/History.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App.css'
+
+const History = () =>{
+    return(
+        <p>historypage</p>
+    )
+}
+
+export default History;
