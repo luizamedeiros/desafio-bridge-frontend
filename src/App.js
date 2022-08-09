9bdfb78e69a5017ae3a331e3dc80ffc67434ae1d
luizamedeiros <65048102+luizamedeiros@users.noreply.github.com>
2022-08-09 16:39:46 -0300

    Improving routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import './App.css';
-import {Routes, Route, BrowserRouter as Router} from 'react-router-dom';
-import Home from './pages/Home';
-import History from './pages/History';
+import {
+  Link, 
+  Route,
+  Routes, 
+  BrowserRouter as Router
+} from 'react-router-dom';
+import {Home} from './pages/Home';
+import {History} from './pages/History';
+import {About} from './pages/About';
 
 function App() {
-return(
+return (
   <>
-  <ul>
-    <li><a href='http://localhost:3000/'>Home</a></li>
-    <li className='headerText'> desafio bridge_ </li>
-    <li><a href='http://localhost:3000/history'>History</a></li>
-  </ul>
   <Router>
+    <ul className='navbar'>
+      <li className='headerText'> desafio bridge_ </li>
+      <li><Link to='/'>Calculadora</Link></li>
+      <li><Link to='/history'>Histórico</Link></li>
+      <li><Link to='/about'>Sobre o desafio</Link></li>
+    </ul>
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
       <Route path="/history" element={<History/>}/>
     </Routes>
   </Router>
