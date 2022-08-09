ab70f2a25c0b03386020b4fc225be7f241b245d4
luizamedeiros <65048102+luizamedeiros@users.noreply.github.com>
2022-08-09 11:39:01 -0300

    Add menu + Router

--- src/App.js
+++ src/App.js
@@ -1,80 +1,22 @@
 import './App.css';
-import Swal from 'sweetalert2';
-import { useState } from 'react';
-import apiService from './services/apiService';
-
+import {Routes, Route, BrowserRouter as Router} from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-
-  const [userInput, setUserInput] = useState();
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if(userInput>1 && Number.isInteger(userInput)){
-      apiService(userInput).then((response) =>{
-        const answer = response.data;
-        Swal.fire({
-          title: `${answer.amount}!`,
-          text: `O número ${userInput} tem ${answer.amount} 
-          número(s) menor(es) que ele, que tem a mesma quantidade de divisores que o seu sucessor direto.
-          Os números que satisfazem essa condição são: ${answer.numbers.slice(1,-1)}.`,
-          confirmButtonColor: '#0094FC',
-        })
-      })
-    }
-    else{
-      Swal.fire({
-        icon: 'error',
-        title: 'Número inválido...',
-        text: 'O número inserido precisa ser um inteiro maior que 1!',
-        confirmButtonColor: '#0094FC',
-        confirmButtonText: 'Ok, vou inserir outro!'
-      })
-    }
-  }
-
-  return (
-        <div className='content-container'>
-            <div className='textDiv'>
-              <div className='content'>
-              <img 
-              src='https://portal.bridge.ufsc.br/wp-content/uploads/2022/06/icone_animado.gif'
-              />
-              <h1 className='title'>
-                desafio bridge_
-              </h1>
-              <p>
-              O desafio proposto foi criar uma aplicação web, que calculasse, 
-              a partir de uma entrada k do usuário,
-              o número de inteiros positivos n menores que k, para os quais
-              n e n + 1 têm o mesmo número de divisores positivos.
-              </p>
-            </div>
-            </div>
-          <div className='inputsDiv'>
-            <form onSubmit={(e)=> onSubmit(e)}>
-              <div className='inputFieldDiv'>
-                <label for='inputField'>
-                  Insira um número:
-                </label>
-                <input 
-                  type="number" 
-                  id="inputField"
-                  name='inputField'
-                  value={userInput}
-                  onChange={(e)=>setUserInput(parseInt(e.target.value))}
-                />
-              </div>
-              <input
-                type='submit'
-                value='Calcular'
-                className='submitButton'
-                placeholder='Insira um número...'
-              />
-            </form>
-          </div>
-        </div>
-  );
+return(
+  <>
+  <ul>
+    <li><a href='http://localhost:3000/'>Home</a></li>
+    <li className='headerText'> desafio bridge_ </li>
+    <li><a >History</a></li>
+  </ul>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+  </Router>
+  </>
+);
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../App.css';
+import Swal from 'sweetalert2';
+import apiService from '../services/apiService';
 
 const Home =()=>{
+    const [userInput, setUserInput] = useState();
 
-    return(
-        <p>
-            hello
-        </p>
-    )
+    const onSubmit = (e) => {
+      e.preventDefault();
+      if(userInput>1 && Number.isInteger(userInput)){
+        apiService(userInput).then((response) =>{
+          const answer = response.data;
+          Swal.fire({
+            title: `${answer.amount}!`,
+            text: `O número ${userInput} tem ${answer.amount} 
+            número(s) menor(es) que ele, que tem a mesma quantidade de divisores que o seu sucessor direto.
+            Os números que satisfazem essa condição são: ${answer.numbers.slice(1,-1)}.`,
+            confirmButtonColor: '#0094FC',
+          })
+        })
+      }
+      else{
+        Swal.fire({
+          icon: 'error',
+          title: 'Número inválido...',
+          text: 'O número inserido precisa ser um inteiro maior que 1!',
+          confirmButtonColor: '#0094FC',
+          confirmButtonText: 'Ok, vou inserir outro!'
+        })
+      }
+    }
+  
+    return (
+          <div className='content-container'>
+              <div className='textDiv'>
+                <div className='content'>
+                <img 
+                src='https://portal.bridge.ufsc.br/wp-content/uploads/2022/06/icone_animado.gif'
+                />
+                <h1 className='title'>
+                  desafio bridge_
+                </h1>
+                <p>
+                O desafio proposto foi criar uma aplicação web, que calculasse, 
+                a partir de uma entrada k do usuário,
+                o número de inteiros positivos n menores que k, para os quais
+                n e n + 1 têm o mesmo número de divisores positivos.
+                </p>
+              </div>
+              </div>
+            <div className='inputsDiv'>
+              <form onSubmit={(e)=> onSubmit(e)}>
+                <div className='inputFieldDiv'>
+                  <label>
+                    Insira um número:
+                  </label>
+                  <input 
+                    type="number" 
+                    id="inputField"
+                    name='inputField'
+                    value={userInput}
+                    onChange={(e)=>setUserInput(parseInt(e.target.value))}
+                  />
+                </div>
+                <input
+                  type='submit'
+                  value='Calcular'
+                  className='submitButton'
+                  placeholder='Insira um número...'
+                />
+              </form>
+            </div>
+          </div>
+    );
 }
+
+export default Home;
