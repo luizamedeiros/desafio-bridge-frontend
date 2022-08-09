9302082455f54397a6c5e044d36f506057b5cf38
luizamedeiros <65048102+luizamedeiros@users.noreply.github.com>
2022-08-09 16:50:47 -0300

    Creating About Page terms

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import './style.css';
+import { 
+    DEPLOY,
+    IMAGE_LINK,
+    TECHNOLOGIES,
+    ABOUT_PARAGRAPH,
+    ABOUT_THE_CHALLENGE,
+ } from './terms.js';
 
 const About = () =>{
     return(
@@ -7,18 +14,18 @@ const About = () =>{
             <div className='textDiv'>
                 <div className='content'>
                     <img 
-                    src='https://portal.bridge.ufsc.br/wp-content/uploads/2022/06/icone_animado.gif'
+                    src={IMAGE_LINK}
                     />
                     <h1>
-                        sobre o desafio_
+                        {ABOUT_THE_CHALLENGE}
                     </h1>
                     <p>
-                    O desafio proposto envolveu a implementação de funcionalidades web tanto no frontend quanto
-                    no backend. <br/> No frontend, foi utilizado React.js, com Sweetalert2 para a criação de modais, 
-                    e React Router Dom para o roteamento.
-                    O backend conta com Spring Boot e Java para o processamento do cálculo e retorno da resposta. <br/>
-                    Tanto o backend quanto a aplicação como um todo tiveram seu deploy feito no heroku.
-                    </p>
+                        {ABOUT_PARAGRAPH}
+                        <br/>
+                        {TECHNOLOGIES}
+                        <br/>
+                        {DEPLOY}
+                   </p>
                 </div>
             </div>
             <div className='linksDiv'>
---
+++ src/pages/About/terms.js
@@ -0,0 +1,9 @@
+export const ABOUT_PARAGRAPH = ` O desafio proposto envolveu a implementação de funcionalidades web tanto no frontend quanto
+no backend.
+`
+export const TECHNOLOGIES = `No frontend, foi utilizado React.js, com Sweetalert2 para a criação de modais, 
+e React Router Dom para o roteamento.
+O backend conta com Spring Boot e Java para o processamento do cálculo e retorno da resposta.`;
+export const DEPLOY = `Tanto o backend quanto a aplicação como um todo tiveram seu deploy feito no heroku.`;
+export const ABOUT_THE_CHALLENGE = `sobre o desafio_`
+export const IMAGE_LINK = `https://portal.bridge.ufsc.br/wp-content/uploads/2022/06/icone_animado.gif`;
