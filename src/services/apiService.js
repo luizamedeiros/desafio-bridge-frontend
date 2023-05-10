59191a2312133d7029cf11771952c8e71b2afcbc
luizamedeiros <luizadmeds@gmail.com>
2023-05-10 18:43:42 -0300

    update backend url

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const API_BASE_URL = "https://dbb-22.herokuapp.com/calculate";
+const API_BASE_URL =
+  "https://desafio-bridge-backend-production.up.railway.app/calculate";
 
 const apiService = (number) => {
   return axios.get(API_BASE_URL, { params: { input: number } });
